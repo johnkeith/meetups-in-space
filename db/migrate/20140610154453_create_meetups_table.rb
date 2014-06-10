d4325cf2c6e6719dd079cdac050c2708993794c5
John Keith <johnkeith@outlook.com>
2014-06-10 11:56:04 -0400

    migration to create meetups

---
+++ db/migrate/20140610154453_create_meetups_table.rb
@@ -0,0 +1,14 @@
+class CreateMeetupsTable < ActiveRecord::Migration
+  def change
+    create_table :meetups do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.string :location, null: false
+      t.integer :created_by
+
+      t.timestamps
+    end
+
+    add_index :meetups, :name, unique: true
+  end
+end
